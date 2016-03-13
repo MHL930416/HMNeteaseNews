9ec4cb0858a83accc97828166b3067d535d821a5
冒黄亮 <MHL@mhl.lan>
2016-03-13 20:29:19 +0800

    下载第三方

---
+++ 网易新闻/网易新闻/HMNetworkingTool.h
@@ -0,0 +1,13 @@
+//
+//  HMNetworkingTool.h
+//  网易新闻
+//
+//  Created by 冒黄亮 on 16/3/13.
+//  Copyright © 2016年 冒黄亮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HMSingleton.h"
+@interface HMNetworkingTool : NSObject
+HMSingleton_h(NetworkingTool)
+@end
---
+++ 网易新闻/网易新闻/HMNetworkingTool.m
@@ -0,0 +1,14 @@
+//
+//  HMNetworkingTool.m
+//  网易新闻
+//
+//  Created by 冒黄亮 on 16/3/13.
+//  Copyright © 2016年 冒黄亮. All rights reserved.
+//
+
+#import "HMNetworkingTool.h"
+
+@implementation HMNetworkingTool
+HMSingleton_m(NetworkingTool)
+
+@end
