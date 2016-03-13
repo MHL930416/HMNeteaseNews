f720ec00a506e958878ecb73dde22d112d7872bb
冒黄亮 <MHL@mhl.local>
2016-03-13 19:17:09 +0800

    初始化项目

---
+++ 网易新闻/网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻
+//
+//  Created by 冒黄亮 on 16/3/13.
+//  Copyright © 2016年 冒黄亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻/网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻
+//
+//  Created by 冒黄亮 on 16/3/13.
+//  Copyright © 2016年 冒黄亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
